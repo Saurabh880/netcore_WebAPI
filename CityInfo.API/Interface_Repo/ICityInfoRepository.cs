d3054f88ac5d56b94937e1c0c8f4af987b5101c6
Saurabh880 <backtohome924@gmal.com>
2025-03-30 22:58:53 +0530

    Added City Interface and service

---
+++ CityInfo.API/Interface_Repo/ICityInfoRepository.cs
@@ -0,0 +1,13 @@
+﻿using CityInfo.API.Entities;
+
+namespace CityInfo.API.Interface_Repo
+{
+    public interface ICityInfoRepository
+    {
+        Task<IQueryable<City>> GetCitiesAsync();
+        Task<City?> GetCity(int cityId);
+        Task<IQueryable<PointOfInterest>> GetPointsOfInterestForCityAsync(int cityId);
+        Task<PointOfInterest?> GetPointOfInterestForCityAsync(int cityId,int pointOfInterestId);
+
+    }
+}
---
+++ CityInfo.API/Services/CityInfoRepository.cs
@@ -0,0 +1,28 @@
+﻿using CityInfo.API.Entities;
+using CityInfo.API.Interface_Repo;
+
+namespace CityInfo.API.Services
+{
+    public class CityInfoRepository : ICityInfoRepository
+    {
+        Task<IQueryable<City>> ICityInfoRepository.GetCitiesAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        Task<City?> ICityInfoRepository.GetCity(int cityId)
+        {
+            throw new NotImplementedException();
+        }
+
+        Task<PointOfInterest?> ICityInfoRepository.GetPointOfInterestForCityAsync(int cityId, int pointOfInterestId)
+        {
+            throw new NotImplementedException();
+        }
+
+        Task<IQueryable<PointOfInterest>> ICityInfoRepository.GetPointsOfInterestForCityAsync(int cityId)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
