509bd42fca4ed862b4d2f20b222bca12ff6e6a40
Saurabh880 <backtohome924@gmal.com>
2025-03-30 21:42:55 +0530

    Added Email Sender Service

---
+++ EmailService/EmailSender.cs
@@ -0,0 +1,74 @@
+﻿using MailKit.Net.Smtp;
+using MailKit.Security;
+using Microsoft.Extensions.Options;
+using MimeKit;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmailService
+{
+    public class EmailSender : IEmailSender
+    {
+        private readonly EmailConfiguration _emailConfig;
+
+        public EmailSender(IOptions<EmailConfiguration> emailConfig)
+        {
+            _emailConfig = emailConfig.Value;
+        }
+
+        public void SendEmail(Message message)
+        {
+            var emailMessage = CreateEmailMessage(message);
+
+            Send(emailMessage);
+        }
+
+        private MimeMessage CreateEmailMessage(Message message)
+        {
+            var emailMessage = new MimeMessage();
+
+            emailMessage.From.Add(new MailboxAddress("email", _emailConfig.From));
+            emailMessage.To.Add(MailboxAddress.Parse(message.To ));
+            emailMessage.Subject = message.Subject;
+            emailMessage.Body = new TextPart(MimeKit.Text.TextFormat.Text) { Text = message.Content };
+
+            return emailMessage;
+        }
+
+        private void Send(MimeMessage mailMessage)
+        {
+            using (var client = new SmtpClient())
+            {
+                try
+                {
+                    //client.Connect(_emailConfig.SmtpServer, _emailConfig.Port, SecureSocketOptions.StartTls);
+                    client.Connect(_emailConfig.SmtpServer, _emailConfig.Port, SecureSocketOptions.StartTls);
+
+                    //client.AuthenticationMechanisms.Remove("XOAUTH2");
+                    client.Authenticate(_emailConfig.From, _emailConfig.Password);
+
+                    client.Send(mailMessage);
+                    Console.WriteLine($"Mail send from {_emailConfig.From}  with MailService.");
+                    Console.WriteLine($"Subject: {mailMessage.Subject}");
+                    Console.WriteLine($"Message: {mailMessage.TextBody}");
+                    //use below link to check mails
+                    //https://code-maze.com/aspnetcore-send-email/
+                    //https://ethereal.email/messages
+                }
+                catch
+                {
+                    //log an error message or throw an exception or both.
+                    throw;
+                }
+                finally
+                {
+                    client.Disconnect(true);
+                    client.Dispose();
+                }
+            }
+        }
+    }
+}
