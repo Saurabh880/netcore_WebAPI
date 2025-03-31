0760ef3a58b3a9b3a0c04fd9dde019edfc715022
Saurabh880 <backtohome924@gmal.com>
2025-04-01 00:04:54 +0530

    Added Repository pattern in API

--- CityInfo.API/Interface_Repo/ICityInfoRepository.cs
+++ CityInfo.API/Interface_Repo/ICityInfoRepository.cs
@@ -4,9 +4,9 @@ namespace CityInfo.API.Interface_Repo
 {
     public interface ICityInfoRepository
     {
-        Task<IQueryable<City>> GetCitiesAsync();
-        Task<City?> GetCity(int cityId);
-        Task<IQueryable<PointOfInterest>> GetPointsOfInterestForCityAsync(int cityId);
+        Task<IEnumerable<City>> GetCitiesAsync();
+        Task<City?> GetCity(int cityId, bool includePointOfInterest);
+        Task<IEnumerable<PointOfInterest>> GetPointsOfInterestForCityAsync(int cityId);
         Task<PointOfInterest?> GetPointOfInterestForCityAsync(int cityId,int pointOfInterestId);
 
     }
--- CityInfo.API/Services/CityInfoRepository.cs
+++ CityInfo.API/Services/CityInfoRepository.cs
@@ -1,28 +1,47 @@
-﻿using CityInfo.API.Entities;
+﻿using CityInfo.API.DbContexts;
+using CityInfo.API.Entities;
 using CityInfo.API.Interface_Repo;
+using Microsoft.EntityFrameworkCore;
 
 namespace CityInfo.API.Services
 {
     public class CityInfoRepository : ICityInfoRepository
     {
-        Task<IQueryable<City>> ICityInfoRepository.GetCitiesAsync()
+        private readonly CityInfoContext _cityInfoContext;
+
+        public CityInfoRepository(CityInfoContext cityInfoContext)
         {
-            throw new NotImplementedException();
+            _cityInfoContext= cityInfoContext ?? throw new ArgumentNullException(nameof(_cityInfoContext ));
+        }
+        public async Task<IEnumerable<City>> GetCitiesAsync()
+        {
+            return await _cityInfoContext.CityInfos.
+                OrderBy(c=>c.Name).ToListAsync();
         }
 
-        Task<City?> ICityInfoRepository.GetCity(int cityId)
+        public async Task<City?> GetCity(int cityId, bool includePointOfInterest)
         {
-            throw new NotImplementedException();
+            if (includePointOfInterest) 
+            {
+                return await _cityInfoContext.CityInfos.
+                    Include( c=> c.PointsOfInterest).
+                    Where(c => c.Id == cityId).FirstOrDefaultAsync();
+            }
+            return await _cityInfoContext.CityInfos.
+                Where(c => c.Id == cityId).FirstOrDefaultAsync();
         }
 
-        Task<PointOfInterest?> ICityInfoRepository.GetPointOfInterestForCityAsync(int cityId, int pointOfInterestId)
+        public async Task<PointOfInterest?> GetPointOfInterestForCityAsync(int cityId, int pointOfInterestId)
         {
-            throw new NotImplementedException();
+            return await _cityInfoContext.PointsOfInterest.
+                Where(p => p.CityId == cityId && p.Id == pointOfInterestId)
+                .FirstOrDefaultAsync();
         }
 
-        Task<IQueryable<PointOfInterest>> ICityInfoRepository.GetPointsOfInterestForCityAsync(int cityId)
+        public async Task<IEnumerable<PointOfInterest>> GetPointsOfInterestForCityAsync(int cityId)
         {
-            throw new NotImplementedException();
+            return await _cityInfoContext.PointsOfInterest.
+                Where(p => p.CityId == cityId).ToListAsync();
         }
     }
 }
